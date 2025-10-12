fcfb71b62dae6135bda13ac4a4d6f60aba3d0b64
KaleChips22 <jonagarm9143@gmail.com>
2025-10-12 19:20:17 -0400

    feat: mouse actions + cursor

--- src/Editor/draw.ts
+++ src/Editor/draw.ts
@@ -6,6 +6,29 @@ const BEAT_HEIGHT = LANE_WIDTH * 4
 let tSigTop = 4
 let tSigBottom = 4
 let yOffset = 0
+let pZoom = 0
+
+let cursorPos = 0
+
+let mouseIsPressed = false
+let pMouseIsPressed = false
+let mouseX: number | null = null
+let mouseY: number | null = null
+
+document.addEventListener('mousemove', (e) => {
+  if ((e.target as HTMLElement).tagName !== 'CANVAS') {
+    mouseX = null
+    mouseY = null
+
+    return
+  }
+
+  mouseX = e.offsetX * window.devicePixelRatio
+  mouseY = e.offsetY * window.devicePixelRatio
+})
+
+document.addEventListener('mousedown', () => (mouseIsPressed = true))
+document.addEventListener('mouseup', () => (mouseIsPressed = false))
 
 document.addEventListener('wheel', (e) => {
   e.preventDefault()
@@ -20,8 +43,37 @@ const draw = (
 ) => {
   const { zoom, division, selectedTool } = globalState
 
-  if (yOffset < (-BEAT_HEIGHT * zoom) / 3) {
-    yOffset = (-BEAT_HEIGHT * zoom) / 3
+  const getBeatFromClick = (mouseY: number): number =>
+    Math.max(0, (height - mouseY + yOffset) / (BEAT_HEIGHT * zoom))
+
+  const getNearestDivision = (beat: number): number => {
+    const divisionsPerBeat = division / tSigBottom
+    return Math.round(beat * divisionsPerBeat) / divisionsPerBeat
+  }
+
+  mouseDown: if (!pMouseIsPressed && mouseIsPressed) {
+    // mouse down event
+    if (mouseX === null || mouseY === null) break mouseDown
+
+    let beatClicked = getBeatFromClick(mouseY)
+
+    cursorPos = getNearestDivision(beatClicked)
+  }
+
+  mouseUp: if (pMouseIsPressed && !mouseIsPressed) {
+    // mouse up event
+    if (mouseX === null || mouseY === null) break mouseUp
+  }
+
+  const beatToY = (beat: number): number =>
+    -(beat * (BEAT_HEIGHT * zoom) - yOffset - height)
+
+  if (yOffset < -150) {
+    yOffset = -150
+  }
+
+  if (pZoom !== zoom && pZoom !== 0) {
+    yOffset *= zoom / pZoom
   }
 
   ctx.clearRect(0, 0, width, height)
@@ -62,20 +114,38 @@ const draw = (
     const maxBigLinesOnScreen = Math.ceil(height / zoomedBeatHeight)
 
     ctx.beginPath()
-    for (let i = 0; i < maxBigLinesOnScreen; i++) {
-      ctx.moveTo(
-        width / 2 - 6 * LANE_WIDTH,
-        (height - (-yOffset + i * zoomedBeatHeight)) %
-          (zoomedBeatHeight * maxBigLinesOnScreen)
-      )
-      ctx.lineTo(
-        width / 2 + 6 * LANE_WIDTH,
-        (height - (-yOffset + i * zoomedBeatHeight)) %
-          (zoomedBeatHeight * maxBigLinesOnScreen)
-      )
-    }
     ctx.strokeStyle = '#bbbf'
+    ctx.fillStyle = '#bbbf'
     ctx.lineWidth = 3
+    ctx.font = '32px Arial'
+    ctx.textAlign = 'right'
+
+    for (let i = 0; i < maxBigLinesOnScreen; i++) {
+      const beatIndex =
+        i +
+        maxBigLinesOnScreen *
+          Math.floor(
+            (height - (-yOffset + i * zoomedBeatHeight)) /
+              (zoomedBeatHeight * maxBigLinesOnScreen)
+          )
+
+      const isMeasureLine = beatIndex % tSigTop === 0
+
+      const y =
+        (height - (-yOffset + i * zoomedBeatHeight)) %
+        (zoomedBeatHeight * maxBigLinesOnScreen)
+      ctx.moveTo(width / 2 - 6 * LANE_WIDTH, y)
+      ctx.lineTo(width / 2 + 6 * LANE_WIDTH, y)
+
+      if (isMeasureLine) {
+        const measureNum = beatIndex / tSigTop + 1
+        ctx.fillText(
+          measureNum.toString(),
+          width / 2 - 6 * LANE_WIDTH - 16,
+          y + 10
+        )
+      }
+    }
     ctx.stroke()
 
     const littleLinesPerBigLine = division / tSigBottom
@@ -103,6 +173,27 @@ const draw = (
   }
 
   drawDivisions()
+
+  const cursorY = beatToY(cursorPos)
+
+  if (cursorY >= 0 && cursorY <= height) {
+    ctx.beginPath()
+    ctx.moveTo(width / 2 - 6 * LANE_WIDTH - 1, cursorY)
+    ctx.lineTo(width / 2 + 6 * LANE_WIDTH, cursorY)
+    ctx.strokeStyle = 'red'
+    ctx.lineWidth = 3
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo(width / 2 - 6 * LANE_WIDTH, cursorY)
+    ctx.lineTo(width / 2 - 6 * LANE_WIDTH - 16, cursorY - 10)
+    ctx.lineTo(width / 2 - 6 * LANE_WIDTH - 16, cursorY + 10)
+    ctx.fillStyle = 'red'
+    ctx.fill()
+  }
+
+  pMouseIsPressed = mouseIsPressed
+  pZoom = zoom
 }
 
 export default draw
