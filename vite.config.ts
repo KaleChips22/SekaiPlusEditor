121eee59f75dfe1cc2c68cbc830583a2ba645436
KaleChips22 <jonagarm9143@gmail.com>
2025-11-04 01:33:34 -0500

    hotfix: build settings, too

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.join(__dirname, 'index.html'),
+        settings: path.join(__dirname, 'settings.html'),
+      },
+    },
+  },
   plugins: [
     react(),
     tailwindcss(),
